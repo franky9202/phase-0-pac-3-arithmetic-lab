992e91e7a460e42814c5206cda71e5b624ffdf59
Edgar <franky9202@gmail.com>
2022-03-09 16:44:44 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+let n = "5"
+function increment(n){
+    return n += 1;
+}
+function decrement(n){
+    return n -= 1;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    console.log(parseFloat("2.222"));
+    console.log(parseFloat('sldkjflksjf'));
+    return parseFloat(n);    
+}
